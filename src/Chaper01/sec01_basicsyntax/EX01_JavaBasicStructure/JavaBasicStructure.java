d3172ec4bf500384ba3490ac842330a486c6b5f7
Jmpak99 <jmpak99@gmail.com>
2025-04-04 21:37:30 +0900

    Chaper01

---
+++ src/Chaper01/sec01_basicsyntax/EX01_JavaBasicStructure/JavaBasicStructure.java
@@ -0,0 +1,14 @@
+package Chaper01.sec01_basicsyntax.EX01_JavaBasicStructure;
+
+/*
+처음 만든 클래스
+(여러 줄 주석)
+*/
+
+
+public class JavaBasicStructure {
+    public static void main(String[] args) {
+        // 1줄 주석 : 화면 코드 출력
+        System.out.println("콘솔 화면에 출력");
+    }
+}
